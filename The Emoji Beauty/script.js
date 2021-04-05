ded17f599901514dfc5deb3a3f4fd35db0ae5708
Asma <60159323+AoCoding-631@users.noreply.github.com>
2021-04-05 16:45:28 -0400

    Add files via upload

---
+++ The Emoji Beauty/script.js
@@ -0,0 +1,58 @@
+
+//face
+function eyes_1()
+{
+    document.getElementById("eyes").src="images/Transparent/e1t.png";
+}
+
+function eyes_2()
+{
+    document.getElementById("eyes").src="images/Transparent/e2t.png";
+}
+
+function eyes_3()
+{
+    document.getElementById("eyes").src="images/Transparent/e3t.png";
+}
+
+
+//lips
+function lips_1()
+{
+    document.getElementById("lips").src="images/Transparent/lip1t.png";
+}
+function lips_2()
+{
+    document.getElementById("lips").src="images/Transparent/lip2t.png";
+}
+function lips_3()
+{
+    document.getElementById("lips").src="images/Transparent/lip3t.png";
+}
+
+//face decals
+function jewel_1()
+{
+    document.getElementById("jewel").src="images/Transparent/fjt1.png";
+
+}   
+function jewel_2()
+{
+    document.getElementById("jewel").src="images/Transparent/fjt2.png";
+
+}   
+function jewel_3()
+{
+    document.getElementById("jewel").src="images/Transparent/fj3t.png";
+
+}   
+function jewel_4()
+{
+    document.getElementById("jewel").src="images/Transparent/fj4t.png";
+
+}   
+function jewel_5()
+{
+    document.getElementById("jewel").src="images/Transparent/fj5t.png";
+
+}   
