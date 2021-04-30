afda1ddef8e5fe8582ddb532ee4923e3fa5008cf
Asma <60159323+AoCoding-631@users.noreply.github.com>
2021-04-30 16:55:03 -0400

    Add files via upload

---
+++ iio/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("#flip-1").click(function(){
+    $(".panel-1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip-2").click(function(){
+    $(".panel-2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip-3").click(function(){
+    $(".panel-3").slideToggle("slow");
+  });
+});
+
+
